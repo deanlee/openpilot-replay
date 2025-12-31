0b7578ebddc6253666d6bc833fb9b6fff6e569b6
Dean Lee <deanlee3@gmail.com>
2025-12-31 22:22:13 +0800

    replace json11 with nlohmann-json (#3)

--- src/qcom_decoder.cc
+++ src/qcom_decoder.cc
@@ -1,7 +1,7 @@
 #include "qcom_decoder.h"
 
 #include <assert.h>
-#include "third_party/linux/include/v4l2-controls.h"
+#include "linux/include/v4l2-controls.h"
 #include <linux/videodev2.h>
 
 #include "common/util.h"
