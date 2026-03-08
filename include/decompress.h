5c42110326883cb92dcb88b083c60cb637a79680
deanlee <deanlee3@gmail.com>
2026-03-09 06:45:57 +0800

    replay: simplify BZ2 and ZST decompression

--- include/decompress.h
+++ include/decompress.h
@@ -4,6 +4,4 @@
 #include <string>
 
 std::string decompressBZ2(const std::string &in, std::atomic<bool> *abort = nullptr);
-std::string decompressBZ2(const std::byte *in, size_t in_size, std::atomic<bool> *abort = nullptr);
 std::string decompressZST(const std::string &in, std::atomic<bool> *abort = nullptr);
-std::string decompressZST(const std::byte *in, size_t in_size, std::atomic<bool> *abort = nullptr);
